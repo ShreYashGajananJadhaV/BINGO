b68e4d7044c4d30182b147729093771bee769799
Shubham Jadhav <sjadhav11999@gmail.com>
2023-05-31 10:40:44 +0530

    webpage changes

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const app = express();
 app.use(bodyparser.urlencoded({extended: true}));
 
 app.get("/",function(request, response){
-    response.sendFile(__dirname + "/index.html");
+    response.sendFile(__dirname + "/webpage/index.html");
 });
 
 app.post("/", function(request, response){
     var num1 = Number(request.body.num1);
     var num2 = Number(request.body.num2);
-    response.sendFile(__dirname+"/index2.html");
+    response.sendFile(__dirname+"/webpage/index2.html");
     
 });
 
